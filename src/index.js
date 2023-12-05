023f386212c36833f88d8d9e2bbfefc1b63bddb8
Yazan Safadi <safadiyazan@gmail.com>
2023-12-05 14:14:35 +0200

    Cesium and Webpack Setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import "../src/css/main.css"
+
+// Your access token can be found at: https://cesium.com/ion/tokens.
+// This is the default access token
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NmYxNjYzNi1kNmQ4LTQzMGEtOGU4Ni1mN2U5OTVlYzc5MmUiLCJpZCI6MTE4MzUyLCJpYXQiOjE2ODM5MDk0OTN9.HrVvhv9eAppSV01COmDor3CGuppPz5iEEtNFeF_wzp8';
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Viewer('cesiumContainer', {
+  terrain: Terrain.fromWorldTerrain(),
+});    
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: CesiumMath.toRadians(0.0),
+    pitch: CesiumMath.toRadians(-15.0),
+  }
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = await createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileset); 
