5004ff8ff20161d62f9e498bd4571e415e9251f4
Yazan Safadi <safadiyazan@gmail.com>
2024-01-07 07:56:12 -0500

    Correct Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white p-3 mt-auto">
       <div className="container-fluid d-flex justify-content-center align-items-center">
-        <p className="mb-0">&copy; 2023, TSMART | Contact: <a href="mailto:laatflowsim@gmail.com" className="text-white">laatflowsim@gmail.com</a></p>
+        <p className="mb-0"> Copyright &copy; 2024 T-SMART, All rights reserved.  <br />  Contact: <a href="mailto:laatflowsim@gmail.com" className="text-white">laatflowsim@gmail.com</a></p>
       </div>
     </footer>
   );
